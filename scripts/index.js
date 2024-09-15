07f1fb8e4d94813814dec88f4f15acfba1e2ca61
Hong Kai <hongkai2287@gmail.com>
2024-09-15 22:23:57 +0800

    Finish Currency Exchange Project

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 import { API_KEY } from "./API_KEY.js";
 import { countryCodes } from "./countryCode.js";
 
-async function getCurrency(){
-    const URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`;
+async function getCurrency(fromCurrency,toCurrency,amount){
+    const URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/pair/${fromCurrency}/${toCurrency}/${amount}`;
 
     const response = await fetch(URL);
 
@@ -13,31 +13,31 @@ async function getCurrency(){
     return await response.json();
 };
 
-async function displayRates(){
+async function displayRates(fromCurrency,toCurrency,amount){
     try{
-        const currencyData = await getCurrency();
+        const currencyData = await getCurrency(fromCurrency,toCurrency,amount);
 
-        console.log(`Base Currency (USD): ${currencyData.conversion_rates.USD}`);
-        console.log(`EUR: ${currencyData.conversion_rates.EUR}`);
-        console.log(`GBP: ${currencyData.conversion_rates.GBP}`);
-        console.log(`JPY: ${currencyData.conversion_rates.JPY}`);
-        console.log(`DZD: ${currencyData.conversion_rates.DZD}`);
+        console.log(currencyData);
+        console.log(`To amount: ${currencyData.conversion_result.toFixed(2)}`);
+        console.log(`From amount: ${amount}` );
+        document.querySelector('.displayExchange').style.visibility = "visible";
+        document.querySelector('.displayExchange-js').textContent = `${amount} ${fromCurrency} = ${currencyData.conversion_result.toFixed(2)} ${toCurrency}`;
     }
     catch (error){
         console.error(error);
     }
 };
 
-displayRates();
-
-
 const selectBox1 = document.querySelector('.selectBox1-js');
 const selectBox2 = document.querySelector('.selectBox2-js');
+const exchangeButton = document.querySelector('.exchangeButton-js');
+const input = document.querySelector('.inputBox-js');
 
 //Add currency symbols to first select box
 countryCodes.forEach(item=>{
     const option = document.createElement('option');
     option.textContent = item;
+    option.value = item;
     selectBox1.appendChild(option);
 });
 
@@ -45,5 +45,41 @@ countryCodes.forEach(item=>{
 countryCodes.forEach(item=>{
     const option = document.createElement('option');
     option.textContent = item;
+    option.value = item;
     selectBox2.appendChild(option);
 });
+
+//when pressing exchange button
+exchangeButton.addEventListener('click',()=>{
+    let inputValue = document.querySelector('.inputBox-js').value;
+    let selectBox1Value = document.querySelector('.selectBox1-js').value;
+    let selectBox2Value = document.querySelector('.selectBox2-js').value;
+    if (inputValue === ''){
+        alert('Please enter a value!');
+    }
+    else if (inputValue < 0){
+        alert('Please input a positive number');
+    }
+    else{
+        displayRates(selectBox1Value,selectBox2Value,inputValue);
+        // console.log(inputValue);
+        // console.log(selectBox1Value);
+        // console.log(selectBox2Value);
+    }
+});
+
+//Adds event listener to input box to check if input has more than
+//2 decimal places
+input.addEventListener('input', (e)=>{
+    const value = e.target.value;
+
+    //check if the number has more than 2 decimal places
+    if (value.includes('.')){
+        const decimalPart = value.split('.')[1];
+        if (decimalPart.length > 2){
+            //Limit the input to 2 decimal places
+            e.target.value = parseFloat(value).toFixed(2);
+        }
+    }
+})
+
